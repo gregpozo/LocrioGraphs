51f77e3d4b254296c6b69c8874a794ee68064614
Gregory Pozo <blacksanin24@gmail.com>
2018-12-12 20:50:52 -0400

    all notch graphs combined

--- notchGraphs.js
+++ notchGraphs.js
@@ -1,4 +1,5 @@
 let myChart = document.getElementById('myChart').getContext('2d');
+let myChart2 = document.getElementById('myChart2').getContext('2d');
 
     // Global Options
     Chart.defaults.global.defaultFontFamily = 'Lato';
@@ -7,7 +8,7 @@ let myChart = document.getElementById('myChart').getContext('2d');
     Chart.defaults.global.defaultFontColor = '#777';
     Chart.defaults.global.responsive = true;
 
-    let massPopChart = new Chart(myChart, {
+    let pieNotch1 = new Chart(myChart, {
       type:'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
       data:{
         labels:['Me Molesta', 'No Me Molesta'],
@@ -16,6 +17,56 @@ let myChart = document.getElementById('myChart').getContext('2d');
           data:[
             20,
             80
+          ],
+          //backgroundColor:'green',
+          backgroundColor:[
+            'rgba(220, 60, 49, 0.8)',
+            'rgba(63,193,115,0.8)',
+            'rgba(235, 156, 58, 0.5)',
+            'rgba(117, 235, 22, 0.5)'
+          ],
+          borderWidth:1,
+          borderColor:'#777',
+          hoverBorderWidth:3,
+          hoverBorderColor:'#000'
+        }]
+      },
+      options:{
+        maintainAspectRatio: false,
+        title:{
+          display:false,
+          text:'Consumo promedio de un Estadounidense durante el "Holiday Season"',
+          fontSize:25
+        },
+        legend:{
+          display:true,
+          labels:{
+            fontColor:'#000'
+          }
+        },
+        //layout:{
+          //padding:{
+            //left:50,
+            //right:0,
+            //bottom:0,
+            //top:0
+          //}
+        //},
+        tooltips:{
+          enabled:true
+        }
+      }
+});
+
+let pieNotch2 = new Chart(myChart2, {
+      type:'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+      data:{
+        labels:['Me Encanta', 'No Me Gusta'],
+        datasets:[{
+          label:'Gasto',
+          data:[
+            53,
+            47
           ],
           //backgroundColor:'green',
           backgroundColor:[
